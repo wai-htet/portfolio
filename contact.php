7ef42c43345e83fba0f4c5dc379eb203295b431e
Htet Htet Wai` <30836774+wai-htet@users.noreply.github.com>
2020-05-08 03:42:24 +0630

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,48 +1,19 @@
-<?php
-$statusMsg = '';
-$msgClass = '';
-if(isset($_POST['submit'])){
-    // Get the submitted form data
-    $email = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+<?php 
+   $name = $_POST['Your name'];
+   $email = $_POST['Your email'];
+   $subject = $_POST['Subject'];
+   $message =$_POST['Your Message'];
+   $From = 'From: https://wai-htet.github.io/wai.github.io/'
+   $to = 'htethw.369@gmail.com'
 
-    // Check whether submitted data is not empty
-    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
-
-        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
-            $statusMsg = 'Please enter your valid email.';
-            $msgClass = 'errordiv';
-        }else{
-            // Recipient email
-            $toEmail = 'htethw.369@gmail.com';
-            $emailSubject = 'Contact Request Submitted by '.$name;
-            $htmlContent = '<h2>Contact Request Submitted</h2>
-                <h4>Name</h4><p>'.$name.'</p>
-                <h4>Email</h4><p>'.$email.'</p>
-                <h4>Subject</h4><p>'.$subject.'</p>
-                <h4>Message</h4><p>'.$message.'</p>';
-
-            // Set content-type header for sending HTML email
-            $headers = "MIME-Version: 1.0" . "\r\n";
-            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-            // Additional headers
-            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
-
-            // Send email
-            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
-                $statusMsg = 'Your contact request has been submitted successfully !';
-                $msgClass = 'succdiv';
-            }else{
-                $statusMsg = 'Your contact request submission failed, please try again.';
-                $msgClass = 'errordiv';
-            }
-        }
-    }else{
-        $statusMsg = 'Please fill all the fields.';
-        $msgClass = 'errordiv';
-    }
+   $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ ?>
+ <?php 
+if ($_POST['submit']) {
+  if(mail($to, $subject, $body, $from)) {
+    echo '<p>Thank you for your email!</p>';
+  } else {
+     echo '<p>Oops! An error occurred. Try sending your message againg.</p>';
+  }
 }
 ?>
